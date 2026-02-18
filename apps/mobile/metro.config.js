fbfe9cfaf5c3a90ddf91a70be4ec1712ded76a51
Heshan Wanigasooriya <heshanmw@gmail.com>
2026-02-18 07:59:59 -0500

    Fix build

---
+++ apps/mobile/metro.config.js
@@ -0,0 +1,21 @@
+const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
+const path = require('path');
+
+const projectRoot = __dirname;
+const monorepoRoot = path.resolve(projectRoot, '../..');
+
+const config = getDefaultConfig(projectRoot);
+
+// Watch all workspace packages
+config.watchFolders = [monorepoRoot];
+
+// Resolve packages from monorepo root node_modules
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(monorepoRoot, 'node_modules'),
+];
+
+module.exports = withNativeWind(config, {
+  input: './global.css',
+});
