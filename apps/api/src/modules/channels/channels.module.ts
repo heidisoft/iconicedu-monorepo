58d597d263f0104f096edf53d81a7a09b4515785
Heshan Wanigasooriya <heshanmw@gmail.com>
2026-04-06 12:57:04 -0400

    Fix api issues

--- apps/api/src/modules/channels/channels.module.ts
+++ apps/api/src/modules/channels/channels.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '@iconicedu/api/modules/auth/auth.module';
 import { ChannelsService } from '@iconicedu/api/modules/channels/channels.service';
 import { ChannelsController } from '@iconicedu/api/modules/channels/channels.controller';
 
 @Module({
+  imports: [AuthModule],
   providers: [ChannelsService],
   controllers: [ChannelsController],
 })
