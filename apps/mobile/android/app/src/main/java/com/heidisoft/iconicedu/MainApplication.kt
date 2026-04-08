8e1acd29c05c1d306591242a4da8c02a09cbc445
Heshan Wanigasooriya <heshanmw@gmail.com>
2026-04-08 18:31:17 -0400

    Fix the icon sizes

--- apps/mobile/android/app/src/main/java/com/heidisoft/iconicedu/MainApplication.kt
+++ apps/mobile/android/app/src/main/java/com/heidisoft/iconicedu/MainApplication.kt
@@ -11,8 +11,6 @@ import com.facebook.react.ReactHost
 import com.facebook.react.common.ReleaseLevel
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
 
-import androidx.appcompat.app.AppCompatDelegate
-
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ExpoReactHostFactory
 
@@ -31,10 +29,6 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
-    // Allow the app UI to follow the system dark/light mode.
-    // Without this call Android defaults to light mode regardless of system setting,
-    // which makes useColorScheme() always return 'light' on Android.
-    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
     DefaultNewArchitectureEntryPoint.releaseLevel = try {
       ReleaseLevel.valueOf(BuildConfig.REACT_NATIVE_RELEASE_LEVEL.uppercase())
     } catch (e: IllegalArgumentException) {
